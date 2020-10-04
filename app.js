a8841bc4d8130e87a2d74be15404d76d2b37d710
droidverine <shivrajpatil285@gmail.com>
2020-10-04 18:55:41 +0100

    Succesfully fetching the images based on breed and reflecting on frontend

--- app.js
+++ app.js
@@ -4,34 +4,50 @@ const resultsSection = document.querySelector('#results');
 // const loading = document.querySelector(".loading-img");
 const imageSection = document.querySelector('.image-section');
 
-const API_URL = 'https://dog.ceo/api/breed/';
+const API_URL='https://dog.ceo/api/breed';
 
+//const API_URL = 'https://dog.ceo/api/breed';
+//https://dog.ceo/api/breeds/image/random
 // loading.style.display = 'none';
 
 form.addEventListener('submit', formSubmitted);
 
 function formSubmitted(event) {
+	console.log("clicked");
     event.preventDefault();
     const searchTerm = input.value;
     getResults(searchTerm);
-    // loading.style.display = '';
 }
 
 function getResults(searchTerm) {
-    const url = `${API_URL}${searchTerm}/images`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => showResults(data.message));
+    //const url = `${API_URL}${searchTerm}/images`;
+    //https://dog.ceo/api/breed/hound/images/random
+    const url = `${API_URL}/${searchTerm}/images`;
+ 
+	fetch(url)
+	.then(response => response.json())
+	.then(response => {
+
+	    console.log(response.message)
+	    showResults(response.message)
+
+});
 }
 
 function showResults(results) {
-    // console.log(results);
-    // const resultImg = document.createElement("img");
-    // resultImg.src = results;
-    results.forEach(dog => {
-        console.log(dog);
-        const imageResult = document.createElement('img');
-        imageResult.src = dog;
-        imageSection.appendChild(imageResult);
-    });
+     console.log(typeof results);
+     var element = document.getElementById("results");
+     const resultImg = document.createElement("img");
+
+     results.forEach(dog => {
+         console.log(dog);
+          src = dog;
+
+          imageSection.appendChild(imageResult);
+          var elem = document.createElement("img");
+    	  elem.setAttribute("src", src);
+      	  elem.setAttribute("height", "150");
+      	  elem.setAttribute("width", "150");
+      	  document.getElementById("results").appendChild(elem);
+     });
 }
